ad8d59aa42d0194eb0f414b89a58a82da49f702a
Ranjithcode <ranjithraffiq1998@gmail.com>
2021-09-29 23:08:44 +0530

    small edit

--- client/src/Component/Dining/index.jsx
+++ client/src/Component/Dining/index.jsx
@@ -5,8 +5,7 @@ function Dining() {
     return (
         <div>
            
-            <h1 className='text-xl my-4 md:my-8 md:text-3xl md:font-semib
-            old lg:px-12' >
+            <h1 className='text-xl my-4 md:my-8 md:text-3xl md:font-semibold lg:px-12' >
                 Dining-Out Restaurant in NCR
             </h1>
             <DiningCrausel/>
@@ -14,4 +13,4 @@ function Dining() {
     )
 }
 
-export de
+export default Dining
